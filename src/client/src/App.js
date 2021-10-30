5cffe830717533dac4ff40f01e2ef282a102f54a
Henawyy <93005811+Henawyy@users.noreply.github.com>
2021-10-30 20:44:35 +0200

    view all flights v1

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CreateFlight from './Admincomponents/CreateFlight';
+import ViewFlights from './Admincomponents/ViewFlights';
+
 
 class App extends Component  {
   render (){
   return (
     <Router>
         <div>
+        <Route exact path='/' component={ViewFlights} />
         <Route path='/create_flight' component={CreateFlight} />
     
         </div>
