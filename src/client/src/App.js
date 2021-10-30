389435393160397d9efda098071258ca3a7a84b9
Ahmedlokma <ahmedlokma22@gmail.com>
2021-10-30 17:04:13 +0200

    lokma

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CreateFlight from './Admincomponents/CreateFlight';
